1c2b4425a11b2e10e8cfdd9f2ad4f9edcad0a369
Fahim Zahur <fahim.zahur@postmates.com>
2017-05-30 11:50:43 -0700

    Add metadate for Hex publish

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule HyperBuffs.Mixfile do
     [app: :hyperbuffs,
      version: "0.1.0",
      elixir: "~> 1.3",
+     description: description(),
+     package: package(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -29,9 +31,24 @@ defmodule HyperBuffs.Mixfile do
   defp deps do
     [
       {:phoenix, "~> 1.2.1"},
-      {:plug_protobufs, github: "hayesgm/plug_protobufs"},
+      {:plug_protobufs, "~> 0.1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:exprotobuf, "~> 1.2"},
       {:poison, ">= 0.0.0"}
+    ]
+  end
+
+  defp description do
+    """ 
+    HyperBuffs is an Elixir library which strongly connects Phoenix to Protobuf definitions. Based on content negotiation from incoming requests, your controllers will seamlessly accept and respond in either JSON or Protobuf.
+    """ 
+  end
+
+  defp package do
+    [
+      maintainers: ["Geoffrey Hayes", "Fahim Zahur"],
+      licenses: ["MIT"], 
+      links: %{"GitHub" => "https://github.com/hayesgm/hyperbuffs"}
     ]
   end
 end
